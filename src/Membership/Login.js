2fffbbc825e26d3a8923e2d684a467b3c5f2faa7
Akshay <choure99@gmail.com>
2021-09-27 23:29:17 +0530

    login page update

--- src/Membership/Login.js
+++ src/Membership/Login.js
@@ -1,66 +1,82 @@
 
-import React from 'react';
-
-//Class Component
+import React, { useReducer } from 'react';
+import "./../style/login.css"
+import api from "../Service/UserService"
+import {Redirect} from "react-router-dom"
+//className Component
 
 class  Login extends React.Component{
 
     constructor(props){  
         //define state for component
         super(props);
+        var redirect=false
+        if(localStorage.getItem("uid")!==null){
+          redirect=true
+        }
         this.state={
-                    username:"",
-                    password:""
+                    emailid:"",
+                    password:"",
+                    red:redirect
         };
     }
 
-
+    onchange=(e)=>{
+      this.setState({[e.target.name]:e.target.value})
+    }
+    userLogin=()=>{
+      if(this.state.emailid!==null||this.state.password!==null||this.state.emailid!==''||this.state.password!==''){
+        api.loginUser(this.state.emailid, this.state.password)
+        .then(resp=>{
+          console.log(resp.data)
+          localStorage.setItem("uid", resp.data.userid);
+          this.setState({red:true});
+          alert("Login Sucessfull!")
+        })
+        .catch(err=>{
+          console.log(err)
+          // alert("Something Wrong!")
+        })
+      }else{
+        alert("All field are required!")
+      }
+    }
     render(){
+      if(this.state.red){
+        return <Redirect to="/"/>
+      }
         return (
-            <div>         
-                    <div className="form-group">
-                        <label htmlFor="email">Email address:</label>
-                        <input type="email"  value={this.state.username}
-                                             onChange={ (event)=>{
-                                                                    this.setState({username: event.target.value})
-                                                                 }
-                                                        } 
-                               className="form-control" 
-                               id="email"/>
-                    </div>
+            <div>    
 
-                    <div className="form-group">
-                        <label htmlFor="pwd">Password:</label>
-                        <input type="password" value={this.state.password} 
-                                               onChange={(event)=>{
-                                                                        this.setState({password: event.target.value})
-                                                                    }
-                                                        }
-                                className="form-control" id="pwd"/>
-                    </div>
+                 <div className="wrapper fadeInDown">
+                  <div id="formContent">
+                <div className="fadeIn first">
+      <img src="http://danielzawadzki.com/codepen/01/icon.svg" id="icon" alt="User Icon" />
+    </div>
 
-                    <div className="checkbox">
-                        <label><input type="checkbox" /> Remember me</label>
-                    </div>
+   
+    {/* <form> */}
+      <input type="email" id="login" className="fadeIn third" name="emailid" placeholder="Email Id" value={this.state.emailid} onChange={this.onchange} />
+      <input type="text" id="password" className="fadeIn third" name="password" placeholder="password" value={this.state.password} onChange={this.onchange} />
+      <input type="submit" className="fadeIn fourth" value="Log In" onClick={this.userLogin}/>
+    {/* </form> */}
 
-                    <button 
-                            className="btn btn-success"
-                            onClick={()=>{
-                                            if(this.state.username =="choure99@gmail.com" 
-                                                &&
-                                                this.state.password=="12345")
-                                            {
-                                                console.log("Valid User");
-                                 
-                                            }
-                                            else{
-                                                console.log("Invalid User");
-                                            }
-                                        }
-                                    }
-                            >Login</button>
-            </div>
+   
+    {/* <div id="formFooter">
+      <a className="underlineHover" href="#">Forgot Password?</a>
+    </div> */}
+
+  </div>
+</div> 
+
+
+                  
+
+            </div>   
         );
     }
 }
 export default Login;
+
+
+
